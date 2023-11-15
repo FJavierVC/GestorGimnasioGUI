69b25a7fc7ab03b20aa52d7092ae552d6440df3d
FJavierVC <126486944+FJavierVC@users.noreply.github.com>
2023-11-15 10:11:20 -0600

    Agregue Clase Interfaz Grafica

---
+++ GestionDeGimnasios/src/main/java/com/mycompany/gestiondegimnasios/Interfaz_Grafica.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.gestiondegimnasios;
+
+/**
+ *
+ * @author Francisco Javier Valdez Cuellar
+ * @author Jose Diego Toledo Muñoz
+ * @author Moises Medina Sanchez
+ * 
+ */
+public class Interfaz_Grafica {
+
+}
